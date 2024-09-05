e2ef60b789d2308e3b253aab48a556ae5cf4a4d7
Kevin Venalainen <kevin.venalainen@around.co>
2024-09-05 17:01:54 +0300

    basics

---
+++ AudioUnitGoogleTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AudioUnitGoogleTest
+//
+//  Created by Kevin Venalainen on 9/5/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
